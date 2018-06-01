46f37b13749300d3be86840c5504dfe8c7a0ed6a
Kyraleese <crystalruby@gmail.com>
2018-06-01 14:35:18 -0400

    REFACTOR
    Since Horizontal search is our Base search, make the specs reflect that
    Move last method in Horizontal up into Base

--- search_methods/base.rb
+++ search_methods/base.rb
@@ -27,5 +27,9 @@ module SearchMethods
       (col..(col + target_word.length - 1)).map{|c| [row, c] }
     end
 
+    def search_result(word, word_location)
+      locs = word_location.map{|loc| "(#{loc[0]},#{loc[1]})"}.join(', ')
+      "#{word}: #{locs}"
+    end
   end
 end
--- search_methods/horizontal.rb
+++ search_methods/horizontal.rb
@@ -2,9 +2,9 @@ require_relative "base.rb"
 
 module SearchMethods
   class Horizontal < Base
-    def search_result(word, word_location)
-      locs = word_location.map{|loc| "(#{loc[0]},#{loc[1]})"}.join(', ')
-      "#{word}: #{locs}"
-    end
+
+    #intentionally left blank; Horizontal search is the Base search; we just use this
+    #name for readability
+
   end
 end
