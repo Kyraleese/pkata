9cd38f5a8c565ec062a7d69a61e33aff66198acb
Kyraleese <crystalruby@gmail.com>
2018-06-01 11:01:41 -0400

    Test 16
    Manipulate the grid properly for a Vertical Search

--- search_methods/vertical.rb
+++ search_methods/vertical.rb
@@ -1,5 +1,9 @@
 module SearchMethods
   class Vertical
+    attr_accessor :grid
 
+    def initialize(grid)
+      @grid = grid.transpose
+    end
   end
 end
--- spec/search_methods/vertical_spec.rb
+++ spec/search_methods/vertical_spec.rb
@@ -1,5 +1,32 @@
 require_relative "../../search_methods/vertical.rb"
 
 describe SearchMethods::Vertical do
+  let(:wordgrid) {
+    [
+      ['R','W','N','H','A','N','A','R','U','A','L','M','S','E','Q'],
+      ['E','I','L','O','W','Y','A','E','X','K','A','R','I','M','U'],
+      ['R','B','V','N','M','I','J','C','P','T','H','U','X','X','Z'],
+      ['K','M','O','E','S','A','T','A','N','I','S','T','A','M','Q'],
+      ['A','A','P','T','R','T','R','J','F','Q','V','S','H','K','J'],
+      ['W','P','L','M','M','I','W','A','M','X','D','A','S','B','L'],
+      ['Y','I','P','Q','G','K','I','K','C','Q','G','A','Z','M','G'],
+      ['N','Q','A','E','U','A','B','N','A','O','O','K','N','U','V'],
+      ['L','U','W','I','E','J','D','T','D','J','L','P','D','Y','P'],
+      ['T','A','S','S','L','E','H','O','F','F','D','U','Q','U','I'],
+      ['Z','T','F','V','P','D','S','P','P','I','M','Y','V','K','T'],
+      ['N','P','M','A','N','O','X','Z','Y','H','O','T','H','N','R'],
+      ['O','A','U','N','C','H','L','H','Z','M','O','M','I','T','Z'],
+      ['L','Y','K','B','H','M','Z','K','O','F','N','L','J','M','U'],
+      ['H','Z','M','N','D','A','A','B','G','J','F','V','E','L','K']
+    ]
+  }
+
+  let(:subject) { SearchMethods::Vertical.new(wordgrid) }
+
+  context "initialization" do
+    it "should load the word grid in the right format" do
+      expect(subject.grid).to eq(wordgrid.transpose)
+    end
+  end
 
 end
