46c5613fccf0a43e2fd43fd3b483a8dcf70ec759
Kyraleese <crystalruby@gmail.com>
2018-05-31 21:33:18 -0400

    Test 1
    Make sure we can load in the word bank

--- spec/wordsearch_spec.rb
+++ spec/wordsearch_spec.rb
@@ -2,4 +2,10 @@ require_relative "../wordsearch.rb"
 
 describe Wordsearch do
 
+  context "initialization" do
+    it "should load the word bank from the file" do
+      wordsearch = Wordsearch.new("spec/data/test.txt")
+      expect(wordsearch.wordbank).to eq(['CARAMON','FLINT','GOLDMOON','LAURANA','RAISTLIN','RIVERWIND','STURM','TANIS','TASSLEHOFF','TIKA'])
+    end
+  end
 end
--- wordsearch.rb
+++ wordsearch.rb
@@ -1,3 +1,8 @@
 class Wordsearch
+  attr_accessor :wordbank
 
+  def initialize(filename)
+    input_file = File.open(filename, 'r')
+    @wordbank = input_file.gets.chomp.split(',')
+  end
 end
