65e43e01b47253e374ac16d622175fd050b836bb
Kyraleese <crystalruby@gmail.com>
2018-06-01 16:23:47 -0400

    Test 25
    Transform the grid for diagonal_down into a horizontally searchable one

--- search_methods/diagonal_down.rb
+++ search_methods/diagonal_down.rb
@@ -2,6 +2,16 @@ require_relative "base.rb"
 
 module SearchMethods
   class DiagonalDown < Base
+    def initialize(grid)
+      @grid = grid
 
+      grid_length = @grid.length
+
+      @grid.each_with_index do |row, i|
+        @grid[i] = Array.new((grid_length - i), ' ') + @grid[i] + Array.new(i, ' ')      
+      end
+
+      @grid = @grid.transpose
+    end
   end
 end
--- spec/search_methods/vertical_spec.rb
+++ spec/search_methods/vertical_spec.rb
@@ -3,12 +3,12 @@ require_relative "../../search_methods/vertical.rb"
 describe SearchMethods::Vertical do
   let(:wordgrid) {
     [
-      ['R','W','N','H','A','N','A','R','U','A','L','M','S','E','Q'],
-      ['E','I','L','O','W','Y','A','E','X','K','A','R','I','M','U'],
-      ['R','B','V','N','M','I','J','C','P','T','H','U','X','X','Z'],
-      ['K','M','O','E','S','A','T','A','N','I','S','T','A','M','Q'],
+      ['R','W','N','H','A','N','A','R','U','A','L','M','S','E','T'],
+      ['E','I','L','O','W','Y','A','E','X','K','A','R','I','M','I'],
+      ['R','B','V','N','M','I','J','C','P','T','H','U','X','X','K'],
+      ['K','M','O','E','S','A','T','A','N','I','S','T','A','M','A'],
       ['A','A','P','T','R','T','R','J','F','Q','V','S','H','K','J'],
-      ['W','P','L','M','M','I','W','A','M','X','D','A','S','B','L'],
+      ['W','P','L','M','M','W','W','A','M','X','D','A','S','B','L'],
       ['Y','I','P','Q','G','K','I','K','C','Q','G','A','Z','M','G'],
       ['N','Q','A','E','U','A','B','N','A','O','O','K','N','U','V'],
       ['L','U','W','I','E','J','D','T','D','J','L','P','D','Y','P'],
@@ -32,16 +32,16 @@ describe SearchMethods::Vertical do
   context "detect word" do
     describe "#index_of_word" do
       it "finds the starting index of word in a list" do
-        list = subject.grid[5]
+        list = subject.grid[14]
         word = 'TIKA'
     
-        expect(subject.index_of_word(list, word)).to eq(4) 
+        expect(subject.index_of_word(list, word)).to eq(0) 
       end
     end
 
     describe "#coordinates_of_word" do
       it "returns the x,y coordinates of the word in grid" do
-        expect(subject.coordinates_of_word("TIKA")).to eq([5,4])
+        expect(subject.coordinates_of_word("TIKA")).to eq([14,0])
       end
     end
 
