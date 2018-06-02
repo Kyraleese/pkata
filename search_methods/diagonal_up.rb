8cb638707efc4b670cdd7910ae02b4d1a886c402
Kyraleese <crystalruby@gmail.com>
2018-06-01 20:43:47 -0400

    Test 29
    Load grid up properly for Diagonal Down

--- search_methods/diagonal_up.rb
+++ search_methods/diagonal_up.rb
@@ -2,6 +2,19 @@ require_relative "base.rb"
 
 module SearchMethods
   class DiagonalUp < Base
+    attr_accessor :original_grid_length
+
+    def initialize(grid)
+      @grid = grid
+
+      @original_grid_length = @grid.length
+
+      @grid.each_with_index do |row, i|
+        @grid[i] = Array.new(i, ' ') + @grid[i] + Array.new((@original_grid_length - i), ' ')
+      end
+
+      @grid = @grid.reverse.transpose
+    end
 
   end
 end
